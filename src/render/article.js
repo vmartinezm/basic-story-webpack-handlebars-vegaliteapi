f23712cee6f76173df3e81fb65af56f684375d94
Administrador <victormartineztrabajo@MacBook-Air-de-Victor-Martinez.local>
2019-04-25 18:05:52 +0200

    prueba de cambio

--- src/render/article.js
+++ src/render/article.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const div = document.createElement('div');
 
-  fetch('http://projects.xocas.com.s3.amazonaws.com/documents/midv_grupo1.json')
+  fetch('http://projects.xocas.com.s3.amazonaws.com/documents/midv_grupo3.json')
   .then((res) => res.json())
   .then((json) => {
     let html = template(json);
